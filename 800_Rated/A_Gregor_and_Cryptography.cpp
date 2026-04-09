13432e035702e5c344e8fa96cb1d6bae48d6a1a2
Prasanna Karagar <prasannakaragar6@gmail.com>
2026-04-09 13:47:48 +0530

    800-Rated

---
+++ 800_Rated/A_Gregor_and_Cryptography.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--){
+      int n;
+      cin>>n;
+      cout<<2<<" "<<n-1<<"\n";
+    }
+    
+    return 0;
+}
