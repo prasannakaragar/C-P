31fa3ec55c218e7ef74f27c351cc079ed5aaa591
Prasanna Karagar <prasannakaragar6@gmail.com>
2026-04-17 23:09:36 +0530

    800_Rated

---
+++ 800_Rated/A_A_Simple_Sequence.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--){
+       int n;
+       cin>>n;
+       while(n){
+        cout<<n--<<" ";
+       }
+       cout<<"\n";
+    }
+    
+
+
+    return 0;
+}
