1b6f3f3e5e0a23d52584595fb7e357ae37a2c860
Prasanna Karagar <prasannakaragar6@gmail.com>
2026-05-27 20:13:51 +0530

    Update cp_Template.cpp

--- cp_Template.cpp
+++ cp_Template.cpp
@@ -1,14 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+#define endl '\n'   
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    int t;
-    cin>>t;
-    while(t--){
 
+    int t;
+    cin >> t;
+    while(t--){
+        
     }
-    
+
     return 0;
-}
