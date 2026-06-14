c11f52e09f266ca7a8857e064be2ca19f4c54b30
Prasanna Karagar <prasannakaragar6@gmail.com>
2026-06-14 19:24:30 +0530

    900_Rated

---
+++ 900_Rated/B_Longest_Divisors_Interval.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--){
+       long long n;
+       cin>>n;
+       int i=1;
+       while( n%i == 0){
+            i++;
+       }
+       cout<<i-1<<endl; 
+    }
+    return 0;
+}
