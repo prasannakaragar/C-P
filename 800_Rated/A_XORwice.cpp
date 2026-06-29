e8f3b66aadbeceeab40b3ca37f5faed5ec53b297
prasannakaragar <prasannakaragar6@gmail.com>
2026-06-29 19:42:23 +0530

    800_Rated

---
+++ 800_Rated/A_XORwice.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'  
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while(t--){
+       int a,b;
+       cin>>a>>b;
+       cout<<(a^b)<<endl;
+    }
+
+    return 0;
+}
