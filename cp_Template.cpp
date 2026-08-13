ae1de0d47d69d2ea102c589ea357133d2fa7aa37
prasannakaragar <prasannakaragar6@gmail.com>
2026-08-13 21:59:54 +0530

    Updated tem

--- cp_Template.cpp
+++ cp_Template.cpp
@@ -1,16 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define endl '\n'  
 
-int main(){
+#define endl '\n'
+
+using ll = long long;
+using vi = vector<int>;
+using vll = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+void solve() {
+
+}
+
+int main() {
     ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(nullptr);
 
     int t;
     cin >> t;
-    while(t--){
-     
-        
+
+    while (t--) {
+        solve();
     }
+
     return 0;
 }
